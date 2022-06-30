fd5f15821289a72d68879329c1ca4e000f9267b6
Vivien Okubo <vivienokubo998@gmail.com>
2022-06-30 15:31:30 -0700

    success

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/bashi
 #include <stdio.h>/*** main - Entry point* Return: Always 0 (Success)*/int main(void){	puts("\"Programming is like building a multilingual puzzle");	return (0);}
