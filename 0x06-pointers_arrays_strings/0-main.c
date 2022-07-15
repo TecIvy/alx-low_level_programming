8928f5e7d39f36162a99ed1c45eb0c71a983fd53
Vivien Okubo <vivienokubo998@gmail.com>
2022-07-15 04:06:07 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -1,14 +1,17 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
-* main - check the code
-* Return: Always 0.
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char s1[98] = "Hello ";
 char s2[] = "World!\n";
 char *ptr;
+
 printf("%s\n", s1);
 printf("%s", s2);
 ptr = _strcat(s1, s2);
